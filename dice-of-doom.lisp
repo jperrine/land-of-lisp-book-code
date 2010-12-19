dee6c4733f06483d373645989bd4ad5f4ce6140e
Jimmy Perrine <jimmy.perrine@gmail.com>
2010-12-18 19:49:31 -0500

    Version 2.0 of dice of doom, memoization, lazy evaluation and game tree pruning and depth evaluation for AI.

--- dice-of-doom.lisp
+++ dice-of-doom.lisp
@@ -1,7 +1,7 @@
 (load "lazy-eval")
 (defparameter *num-players* 2)
 (defparameter *max-dice* 3)
-(defparameter *board-size* 4)
+(defparameter *board-size* 5)
 (defparameter *board-hexnum* (* *board-size* *board-size*))
 (defparameter *ai-level* 4)
 
@@ -176,15 +176,12 @@
 
 (defun rate-position (tree player)
 	(let ((moves (caddr tree)))
-		(if moves
-				(apply (if (eq (car tree) player)
-									 #'max
-									 #'min)
-							 (get-ratings tree player))
-				(let ((w (winners (cadr tree))))
-					(if (member player w)
-							(/ 1 (length w))
-						0)))))
+		(if (not (lazy-null moves))
+		(apply (if (eq (car tree) player)
+						#'max
+						#'min)
+				(get-ratings tree player))
+		(score-board (cadr tree) player))))
 						
 (let ((old-rate-position (symbol-function 'rate-position))
 			(previous (make-hash-table)))
@@ -196,13 +193,49 @@
 					(setf (gethash tree tab)
 								(funcall old-rate-position tree player))))))
 
-(defun get-ratings (tree player)
-	(mapcar (lambda (move)
-						(rate-position (cadr move) player))
-					(caddr tree)))
+(defun ab-get-ratings-max (tree player upper-limit lower-limit)
+	(labels ((f (moves lower-limit)
+							(unless (lazy-null moves)
+								(let ((x (ab-rate-position (cadr (lazy-car moves))
+																						player
+																						upper-limit
+																						lower-limit)))
+									(if (>= x upper-limit)
+											(list x)
+											(cons x (f (lazy-cdr moves) (max x lower-limit))))))))
+		(f (caddr tree) lower-limit)))
+		
+(defun ab-get-ratings-min (tree player upper-limit lower-limit)
+	(labels ((f (moves upper-limit)
+							(unless (lazy-null moves)
+								(let ((x (ab-rate-position (cadr (lazy-car moves))
+																						player
+																						upper-limit
+																						lower-limit)))
+									(if (<= x lower-limit)
+											(list x)
+											(cons x (f (lazy-cdr moves) (min x upper-limit))))))))
+		(f (caddr tree) upper-limit)))
+		
+(defun ab-rate-position (tree player upper-limit lower-limit)
+	(let ((moves (caddr tree)))
+		(if (not (lazy-null moves))
+			(if (eq (car tree) player)
+					(apply #'max (ab-get-ratings-max tree
+																						player
+																						upper-limit
+																						lower-limit))
+					(apply #'min (ab-get-ratings-min tree
+																						player
+																						upper-limit
+																						lower-limit)))
+			(score-board (cadr tree) player))))
 					
 (defun handle-computer (tree)
-	(let ((ratings (get-ratings (limit-tree-depth tree *ai-level*) (car tree))))
+	(let ((ratings (ab-get-ratings-max (limit-tree-depth tree *ai-level*)
+																			(car tree)
+																			most-positive-fixnum
+																			most-negative-fixnum)))
 		(cadr (lazy-nth (position (apply #'max ratings) ratings) (caddr tree)))))
 		
 (defun play-vs-computer (tree)
@@ -210,3 +243,23 @@
 	(cond ((lazy-null (caddr tree)) (announce-winner (cadr tree)))
 				((zerop (car tree)) (play-vs-computer (handle-human tree)))
 				(t (play-vs-computer (handle-computer tree)))))
+				
+(defun score-board (board player)
+	(loop for hex across board
+				for pos from 0
+				sum (if (eq (car hex) player)
+								(if (threatened pos board)
+										1
+										2)
+								-1)))
+								
+(defun threatened (pos board)
+	(let* ((hex (aref board pos))
+					(player (car hex))
+					(dice (cadr hex)))
+		(loop for n in (neighbors pos)
+					do (let* ((nhex (aref board n))
+										(nplayer (car nhex))
+										(ndice (cadr nhex)))
+								(when (and (not (eq player nplayer)) (> ndice dice))
+									(return t))))))
